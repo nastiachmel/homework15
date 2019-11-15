5f44ccab68e045fcd147eb838f2caac0a633ddae
nastiachmel <nastiachmel@gmail.com>
2019-11-15 15:31:44 +0200

    fix

--- part1/js/main.js
+++
@@ -1,9 +0,0 @@
-
-(function($){
-  $(function() {
-    $('.menu__icon').on('click', function() {
-      $(this).closest('.menu')
-        .toggleClass('menu_state_open');
-    });
-      });
-})(jQuery);
